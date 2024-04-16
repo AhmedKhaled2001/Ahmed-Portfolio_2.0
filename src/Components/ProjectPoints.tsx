95e551cb7fb3198c982ce1600409b50e2d7fb955
AhmedKhaled2001 <dido17777@gmail.com>
2024-04-16 12:55:39 -0600

    thoughts page init

---
+++ src/Components/ProjectPoints.tsx
@@ -0,0 +1,55 @@
+function ProjectPoints() {
+  return (
+    <div>
+    <div className="row ">
+      <div className="col ">
+        <div className="container">
+          <p className="project-list-desc project-points-l"> Project Type </p>
+        </div>
+      </div>
+      <div className="col ">
+        <div className="container">
+          <p className="project-list-desc project-points-r"> Personal Project </p>
+        </div>
+      </div>
+    </div>
+    <div className="row ">
+      <div className="col ">
+        <div className="container">
+          <p className="project-list-desc project-points-l"> Project Progress </p>
+        </div>
+      </div>
+      <div className="col ">
+        <div className="container">
+          <p className="project-list-desc project-points-r"> Under Development </p>
+        </div>
+      </div>
+    </div>
+    <div className="row ">
+      <div className="col ">
+        <div className="container">
+          <p className="project-list-desc project-points-l"> Technology </p>
+        </div>
+      </div>
+      <div className="col ">
+        <div className="container">
+          <p className="project-list-desc project-points-r"> Unreal Engine 5 C++/Blueprints </p>
+        </div>
+      </div>
+    </div>
+    <div className="row ">
+      <div className="col ">
+        <div className="container">
+          <p className="project-list-desc project-points-l"> Team Size </p>
+        </div>
+      </div>
+      <div className="col ">
+        <div className="container">
+          <p className="project-list-desc project-points-r"> Solo  </p>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
+export default ProjectPoints;
--- src/Pages/ThoughtsPage.tsx
+++ src/Pages/ThoughtsPage.tsx
@@ -1,6 +1,21 @@
+import ProjectList from "../Components/ProjectList";
+import Tag from "../Components/Tag";
+
 function ThoughtsPage() {
-    return (
-        <p>Hello World</p>
-    );
+  return (
+    <div className=" ProjectPage">
+
+      <div>
+        <div className="row ">
+          <div className="col ">
+            <div className="container">
+            <ProjectList/>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default ThoughtsPage;
