8a2723c30ce9d1e04d9e1fdb5166f9ab1f373871
AhmedKhaled2001 <dido17777@gmail.com>
2024-12-31 23:21:14 -0700

    resume update

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -71,7 +71,7 @@ function NavBar({
               <a
                 className="text-nav1"
                 target="_blank" rel="noopener noreferrer"
-                href="https://drive.google.com/file/d/1siHwZNcruUptN3sUMut0Gx3vL_6CXFdu/view?usp=sharing"
+                href="https://drive.google.com/file/d/1huvga1ZsAO8w-u0Ka5Nz7x4rawbTfPbK/view?usp=sharing"
               >
                 Resume
               </a>
